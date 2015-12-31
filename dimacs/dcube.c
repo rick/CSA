598dd9920641b0d72584dae4fa40a590f11232f9
Rick Bradley <rick@rickbradley.com>
2015-12-31 16:51:16 -0500

    Provide declaration for strcpy()

--- dimacs/dcube.c
+++ dimacs/dcube.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+#include<string.h>
 
 /*-----------------------------------------------------------*/
 /*  Generates instances for geometric matching in   DIMACS    */ 
